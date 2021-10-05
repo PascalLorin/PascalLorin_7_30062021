c4a4d49c06d07201ed6834d3ad04a736b890d6a8
Lorin Pascal <pascal@bricadabra.fr>
2021-10-05 17:13:35 +0200

    quick commit to switch to main

--- pages/iterativeSearch.js
+++ pages/iterativeSearch.js
@@ -1,10 +1,10 @@
-// search input value in all the recipes's name & description and ingredientSet
-// return true if found
+// recherche itérative dans l'intitulé, la description et les ingrédients de toutes les recettes
+// retourne true si trouvé
 function iterativeSearch(item, wItem) {
   let found = false
   recipeSet.forEach(w => {
     let tagToAdd = false
-    if (w.upperDescription.includes(wItem) || w.upperName.includes(wItem)) {
+    if (w.upperDescription.includes(wItem) || w.upperName.includes(wItem) || w.ingredients) {
       found = true
       taggedRecipes.push(w.id)
       let wTag = tagSet[tagSet.length - 1]
@@ -14,7 +14,7 @@ function iterativeSearch(item, wItem) {
         if (wItem.includes(wTag.upperName)) {
           if (wTag.upperName != wItem) {
             let parent = document.getElementById(wTag.name)
-            parent.setAttribute('id',item)
+            parent.setAttribute('id', item)
             lastSearchTagName.textContent = item
             tagSet[tagSet.length - 1].name = item
             tagSet[tagSet.length - 1].upperName = wItem
@@ -40,15 +40,6 @@ function iterativeSearch(item, wItem) {
       }
     }
   })
-  // recherche itérative du tag saisi dans l'array des ingrédients
-  ingredientSet.forEach(ingr => {
-    if (ingr.upperName.includes(wItem)) {
-      found = true
-      ingr.recipes.forEach(r => {
-        taggedRecipes.push(r)
-      })
-    }
-  })
   return found
 }
 
@@ -61,23 +52,19 @@ function selectSearch(event) {
     case "Escape":
       reInitPage()
       return
-    case "Backspace":
-      treatLastTag0()
-      treatTags()
-      break
   }
   if (search.value.length > 2) {
     let tSearch = search.value.split(" ")
     let wSearch = toUpperName(search.value)
     let wString = wSearch.split(" ")
     let l = wString.length
+    taggedRecipes = []
     for (let i = 0; i < l; i++) {
       if (tSearch[i].length > 2) {
         if (l > 1 && i < l - 1) {
           tSearch[i] += " "
           wString[i] += " "
         }
-        taggedRecipes = []
         if (iterativeSearch(tSearch[i], wString[i])) {
           selRecipes()
           displaySelRecipes()
